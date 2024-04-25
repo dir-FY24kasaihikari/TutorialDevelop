4b218b36152d33a9be7f7325d2b9d81acb6899d9
dir-FY24kasaihikari <hikari.kasai.k885@dirbato.co.jp>
2024-04-25 10:25:12 +0900

    新規User登録

--- TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional; // 追加
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -18,5 +19,11 @@ public class UserService {
     public List<User> getUserList(){
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
+    }
+    
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 }
